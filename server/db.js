60b92406b91a7b3c6b806afc4bf8ddd5cfde3e0b
Rohit patidar <159720146+Rohitpatidar1@users.noreply.github.com>
2025-02-24 18:30:58 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
+    await mongoose.connect(
       "mongodb+srv://rrohitpatidar1234:Rohit%40123@cluster0.b7oz7.mongodb.net/textdb?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
@@ -12,7 +12,7 @@ const connectDB = async () => {
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error);
-    process.exit(1);
+    // Yaha process.exit(1); hata diya hai
   }
 };
 
